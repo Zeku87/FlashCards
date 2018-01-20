1bc023948a4cc30c6542d3aea03d20420cbf2060
zeku87 <nocomment182@gmail.com>
2018-01-20 13:55:39 +0100

    404 error handler

--- app.js
+++ app.js
@@ -58,6 +58,18 @@ app.post("/hello", (req, res) => {
 		res.redirect("/");
 });
 
+app.use((req, res, next) => {
+	const err = new Error("¡¡Te has salido del tiesto!!");
+	err.status = 404;
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	res.locals.error = err;
+	res.status(404);
+	res.render('error');
+});
+
 app.listen(3000, () => {
 	console.log("Conectado al puerto 3000");
 });
