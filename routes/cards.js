fd4b5c7dd0b5f3b08f68bc5cf19c1b6c60233e52
zeku87 <nocomment182@gmail.com>
2018-01-23 11:55:13 +0100

    Sign in with simple validation and cookies

--- routes/cards.js
+++ routes/cards.js
@@ -14,26 +14,29 @@ const {cards} = data; // equivale a cards = data.cards
 
 //Ruta donde se visualizan todas las cards
 router.get("/:id", (req, res) => {
-//http://example.com/resource?building=42&age=40&color=salmon
+	const username = req.cookies.username;
 	
+	if(typeof(username) == 'undefined') //return previene que la función siga su curso
+		return res.redirect('/hello');
+
 	let cardDataTemplate = {};
 	const {id} = req.params;
 	const {side} = req.query; //side puede contener "question" o "answer" o undefined
-	
+
 	if( side )
 	{	
 		const text = cards[id][side];
 		const hint = cards[id]["hint"];
 	
 		if(side === 'question')
-			cardDataTemplate = { id, text, hint};
+			cardDataTemplate = { id, text, hint, username};
 		if(side === 'answer')
-			cardDataTemplate = { id, text };
+			cardDataTemplate = { id, text, username};
 	}
 	else{
 		const text = cards[id]["question"];
 		const hint = cards[id]["hint"];
-		cardDataTemplate = {id, text, hint};
+		cardDataTemplate = {id, text, hint, username};
 	}
 
 	res.render("card", cardDataTemplate);
@@ -42,6 +45,11 @@ router.get("/:id", (req, res) => {
 
 //Ruta /cards saca un numero aleatorio(una card aleatoria) y redireccionamos
 router.get("/", (req, res) => {
+		const username = req.cookies.username;
+		
+		if(typeof(username) == 'undefined')//si no ponemos return sigue leyendo instrucciones
+			return res.redirect('/hello');
+
 		const cardId = Math.floor(Math.random() * cards.length);
 		res.redirect(`/cards/${cardId}?side=question`);
 });
